59f2ec98962716898b85aed070e5b14d48996884
Kaizhuo Chen <kaizhuo@ad.unc.edu>
2022-06-01 16:02:37 -0400

    Add globe and change kitchen background

--- pages/index.js
+++ pages/index.js
@@ -2,6 +2,8 @@ import React from 'react';
 import {useRouter} from 'next/router'
 import {useWrapperContext} from '../context/context'
 import translations from '../public/text/translations'
+import Image from 'next/image'
+import style from '../styles/other.module.css'
 
 //Index with start button to go to intro dialog
 export default function Index() {
@@ -10,6 +12,18 @@ export default function Index() {
   const router = useRouter()
 
   return (
-    <button className="start_button" onClick={() => router.push('/intro') }>{translations.start[lang]}</button>
+    <div>
+      <div id={style.global} >
+        <Image
+        width = {"300px"}
+        height = {"280px"}
+        quantity = {100}
+        priorityv = {true}
+        src={"/img/other/global.png"}/>
+      </div>
+      <div>
+        <button className="start_button" onClick={() => router.push('/intro') }>{translations.start[lang]}</button>
+      </div>    
+    </div>  
   )
 }
