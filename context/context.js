2525db6f0340815de674b79de62064fcd45332fe
johnleh <johnleh@live.unc.edu>
2022-09-14 15:55:28 -0400

    lang and avatar now saved to local storage

--- context/context.js
+++ context/context.js
@@ -1,4 +1,4 @@
-import React, { useState,createContext, useContext } from 'react';
+import React, { useState,createContext, useContext ,useEffect} from 'react';
 
 //creating a context with useContext react hook
 const Context = createContext();
@@ -6,13 +6,33 @@ const Context = createContext();
 export default function ContextWrapper({ children }) {
   //creating the context states
   //lang is the current langage needed to show
-  const [lang, setLang] = useState("en");
+  const [lang, setLang] = useState("es");
+
+  useEffect(() => {
+    //init lang on refresh
+    const langState = window.localStorage.getItem('LANG_STATE');
+    if ( langState === null) {
+      window.localStorage.setItem('LANG_STATE',"en")
+      setLang("en")
+    } else {
+      setLang(window.localStorage.getItem('LANG_STATE'))
+    }
+
+    //init avatar on refresh
+    const avatarState = window.localStorage.getItem('AVATAR_STATE');
+    if ( avatarState === null) {
+      window.localStorage.setItem('AVATAR_STATE',"es")
+      setAvatar("1")
+    } else {
+      setAvatar(window.localStorage.getItem('AVATAR_STATE'))
+    }
+  });
+
   //avatar keeps track of what avatar to show
-  //TODO: save this so that when there is a refresh it does not reset to 1
   const [avatar, setAvatar] = useState("1");
   //questionNum remebers which question number a user was on even if there is a lang switch
   const [questionNum, setQuestionNum] = useState(0);
-
+  const [pageState, setPageState] = useState(0);
   //this is what is stored in the context
   let value = {
       state: {
@@ -20,8 +40,14 @@ export default function ContextWrapper({ children }) {
           questionNum: questionNum,
           avatar:avatar,
       },
-      setLang: (newLang) => setLang(newLang),
-      setAvatar: (newAvatar) => setAvatar(newAvatar),
+      setLang: (newLang) => {
+        window.localStorage.setItem('LANG_STATE',newLang)
+        setLang(newLang)
+      },
+      setAvatar: (newAvatar) => {
+        window.localStorage.setItem('AVATAR_STATE',newAvatar)
+        setAvatar(newAvatar)
+      },
       setQuestionNum: (newNum) => setQuestionNum(newNum),
   }
 
