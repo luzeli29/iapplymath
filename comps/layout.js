1c2b1b8cce6901d18d82d03c4a77b8728b0c763a
johnleh <johnleh@live.unc.edu>
2022-12-18 18:01:01 -0500

    Reverted css

--- comps/layout.js
+++ comps/layout.js
@@ -1,39 +1,16 @@
 import Header from "./header";
-import {useWrapperContext} from '../context/context'
-
-//standard layout of every page
-//has a header with title and lang select,
-//then the children displayed in the box of the view container
 export default function Layout({ children }) {
-    const context = useWrapperContext();
-
+    //standard layout of every page
+    //has a header with title and lang select,
+    //then the children displayed in the box of the view container
     return ( 
         <>
-            <div className="">
+            <div className="header_container">
                 <Header/>
             </div>
-            <div className="row justify-content-lg-center">
-                <div className="col border">
-                    {
-                        process.env.NODE_ENV === 'development' ?
-                        <div className='text-center'>
-                            <p><strong>Debug Menu</strong></p>
-                            <p>Lang: {context.state.lang}</p>
-                            <p>AvatarID: {context.state.avatarID}</p>
-                            <p>User ID: {context.state.userID}</p>
-
-                        </div>
-                        :
-                        <>
-                        </>
-                    }
-                </div>
-                <div className="col border">
-                    <div className="view_container">
-                        { children }
-                    </div>
-                </div>
-                <div className="col border">
+            <div className="body_container">
+                <div className="view_container">
+                    { children }
                 </div>
             </div>
         </>
