865ee20407e79b1bc9f0de7f3fb21165b310ea12
Santos Lopez-Lucas <69735829+slopezlucas@users.noreply.github.com>
2023-03-04 07:10:53 -0500

    added set A, B, and some of C & D

--- utils/game/restaurant/generate_restaurant_question.js
+++ utils/game/restaurant/generate_restaurant_question.js
@@ -1,5 +1,6 @@
 //Generate questions with the order state
 import createGameQuestion from '@utils/game/create_game_question.js'
+import { simplifyFraction } from "@common_imports"
 
 function generateOrderQuestions(order) {
     var questions = [];
@@ -212,7 +213,7 @@ function generateOrderQuestions(order) {
         {
             en:"Your budget is now " + secondRandomValue + " dollars. If you order the $" + order.drink.price + " " + order.drink.en +
                 " and $" + order.dessert.price + " " + order.dessert.en +  ", how much money do you have left over?",
-            es:"Su presupesto es ahora "  + secondRandomValue + " dólares. Si ordena la bebida de " + order.drink.price + " dólares y el postre de " + order.dessert.price + "dólares, " +
+            es:"Su presupesto es ahora "  + secondRandomValue + " dólares. Si ordena la bebida de " + order.drink.price + " dólares y el postre de " + order.dessert.price + " dólares, " +
                 "¿cuánto dinero le sobra?"
             ,
         },
@@ -251,7 +252,7 @@ function generateOrderQuestions(order) {
             en:"How much will you pay for the entrée," + order.entree.en + ", if you share it with " + randomValue + " friends and you all equally split the cost?",
             es:"¿Cuánto pagará por el plato principal, " + order.entree.es + ", si lo comparte con " + randomValue + " amigos y todos dividen el costo por igual?",
         },
-        order.entree.price / (randomValue + 1),
+        simplifyFraction(order.entree.price, (randomValue + 1)),
         [{
             en: "(" + order.entree.en + ") ÷ (Total number of people) = ???",
             es: "(" + order.entree.es + ") ÷ (Número total de personas) = ???",
@@ -282,6 +283,92 @@ function generateOrderQuestions(order) {
         }],
         "decimal"
     )
+
+    questions[17] = createGameQuestion(
+        {
+            en:"Elena ordered the same entrée, " + order.entree.en + ", as you. What is the total cost of the two entrées?",
+            es:"Elena pidió la misma entrada, "  + order.entree.es + ", que tú. ¿Cuál es el costo total de las dos entradas?",
+        },
+        order.entree.price * 2,
+        [{
+            en: "(" + order.entree.en + ") + (" + order.entree.en + ") = Total cost OR (" + order.entree.en + ") x 2 = Total cost",
+            es: "(" + order.entree.es + ") + (" + order.entree.es + ") = Costo total O (" + order.entree.es + ") x 2 = Costo total",
+        },{
+            en: "(" + order.entree.en + ") + (" + order.entree.en + ") = " + order.entree.price * 2 + " OR (" + order.entree.en + ") x 2 = " + order.entree.price * 2,
+            es: "(" + order.entree.es + ") + (" + order.entree.es + ") = " + order.entree.price * 2 + " O (" + order.entree.es + ") x 2 = " + order.entree.price * 2,
+        }],
+        "decimal"
+    )
+    questions[17] = createGameQuestion(
+        {
+            en:"Elena wants to order some food to bring home to her family. She ordered the same entrée and dessert as you for her father, mother, and brother. How much will this food cost Elena?",
+            es:"Elena quiere pedir algo de comida para llevar a casa a su familia. Pidió la misma entrada y postre que tú para su padre, su madre y su hermano. ¿Cuánto le costará a Elena esta comida?",
+        },
+        order.entree.price * 3 + order.dessert.price * 3,
+        [{
+            en: "Sum of Entrees + Sum of Desserts = Total Cost",
+            es: "Suma de entradas + Suma de postres = Costo total",
+        },{
+            en: order.entree.price + " + " + order.entree.price + " + " + order.entree.price + " + " + order.dessert.price + " + " + order.dessert.price + " + " + order.dessert.price + " = " + (order.entree.price * 3 + order.dessert.price * 3),
+            es: order.entree.price + " + " + order.entree.price + " + " + order.entree.price + " + " + order.dessert.price + " + " + order.dessert.price + " + " + order.dessert.price + " = " + (order.entree.price * 3 + order.dessert.price * 3),
+        }],
+        "decimal"
+    )
+    questions[18] = createGameQuestion(
+        {
+            en:"Elena’s mother and father did not want a dessert. What is the total cost for Elena’s order after removing her parents’ desserts?",
+            es:"La madre y el padre de Elena no querían postre. ¿Cuál es el costo total del pedido de Elena después de eliminar los postres de sus padres?",
+        },
+        order.entree.price * 3 + order.dessert.price * 1,
+        [{
+            en: "Initial Cost – ("+ order.dessert.en + " x 2) = New Cost  OR  Initial Cost – ("+ order.dessert.en + " + "+ order.dessert.en + ") = New Cost",
+            es: "Costo inicial – ("+ order.dessert.en + " x 2) = Nuevo costo  O  Costo inicial – ("+ order.dessert.en + " + "+ order.dessert.en + ") = Nuevo costo",
+        },{
+            en: order.entree.price * 3 + order.dessert.price * 3 + " - " + order.dessert.price * 2 + " = " + (order.entree.price * 3 + order.dessert.price * 1),
+            es: order.entree.price * 3 + order.dessert.price * 3 + " - " + order.dessert.price * 2 + " = " + (order.entree.price * 3 + order.dessert.price * 1),
+        }],
+        "decimal"
+    )
+    questions[19] = createGameQuestion(
+        {
+            en:"Elena would like to taste half of your entrée. How much of your food will you be sharing with her? Hint: write your answer as a fraction.",
+            es:"A Elena le gustaría probar la mitad de tu entrada. ¿Cuánto de tu comida compartirás con ella? Pista: escribe tu respuesta como una fracción.",
+        },
+        simplifyFraction(1, 2),
+        [{
+            en: "Half = ½",
+            es: "Mitad = ½",
+        }],
+        "decimal"
+    )
+    randomValue = getRandomInt(0, 2);
+    let fractionArrayEN = ["third", "fourth", "sixth"];
+    let fractionArrayES = ["tercio", "cuarto", "sexto"];
+    questions[20] = createGameQuestion(
+        {
+            en:"Elena would like to taste a " + fractionArrayEN[randomValue] + "of your entrée. How much of your food will you be sharing with her? Hint: write your answer as a fraction.",
+            es:"A Elena le gustaría probar un " + fractionArrayES[randomValue] + " octava parte de tu plato principal. ¿Cuánto de tu comida compartirás con ella? Pista: escribe tu respuesta como una fracción.",
+        },
+        randomValue == 0 ? simplifyFraction(1 / 3) : randomValue == 1 ? simplifyFraction(1 / 4) : simplifyFraction(1 / 6),
+        [{
+            en: randomValue == 0 ? fractionArrayEN[randomValue] + " = 1/3" : randomValue == 1 ? fractionArrayEN[randomValue] + " = 1/4" : fractionArrayEN[randomValue] + " = 1/6",
+            es: randomValue == 0 ? fractionArrayES[randomValue] + " = 1/3" : randomValue == 1 ? fractionArrayES[randomValue] + " = 1/4" : fractionArrayES[randomValue] + " = 1/6",
+        }],
+        "decimal"
+    )
+    questions[21] = createGameQuestion(
+        {
+            en:"Elena would like to taste an eighth of your entrée. How much of your food will you be sharing with her? Hint: write your answer as a fraction.",
+            es:"A Elena le gustaría probar una octava parte de tu plato principal. ¿Cuánto de tu comida compartirás con ella? Pista: escribe tu respuesta como una fracción.",
+        },
+        simplifyFraction(1, 8),
+        [{
+            en: "Eigth = 1/8",
+            es: "Octavo = 1/8",
+        }],
+        "decimal"
+    )
+
     return questions
 
 }
