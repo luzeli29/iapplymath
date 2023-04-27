3fde259a5769f04cb981949c91078ba15ca71e83
esc20936 <esc20936@uvg.edu.gt>
2023-04-27 15:46:44 -0600

    TTS in question Boxes

--- comps/accessibility/textReader.js
+++ comps/accessibility/textReader.js
@@ -1,15 +1,16 @@
-import { React, useState } from 'react'
+import { React, useEffect, useState } from 'react'
 import { MdHearing } from "react-icons/md";
 import { useUserContext } from '@hooks/siteContext/useUserContext';
 import { useRouter } from 'next/router';
 
 
 export default function TextReader({ text, reader }) {
+    
     const { user, settings, loading, error } = useUserContext()
+    const [supportsSpeechSynthesis, setSupportsSpeechSynthesis] = useState(true)
     const [isSpeaking, setIsSpeaking] = useState(false)
     const router = useRouter()
 
-    
 
     if (loading || !router.isReady) return <Loading />
     if (error) return <Error error={error} />
@@ -21,8 +22,16 @@ export default function TextReader({ text, reader }) {
 
     if (typeof window !== "undefined") {
         synth = window.speechSynthesis;
-        utterance = new SpeechSynthesisUtterance(text);
-        setUtteranceByReader(reader, utterance, synth, lang);
+
+        if (!synth) {
+            setSupportsSpeechSynthesis(false)
+            alert("Your browser does not support speech synthesis. Please use a different browser.")
+            return <></>
+        }
+        console.log("reader:", reader)
+        reader = reader ? reader : "default";
+
+        utterance =setUtteranceByReader(reader, synth, lang,text,reader);
 
         utterance.onend = function () {
             setIsSpeaking(false);
@@ -36,6 +45,7 @@ export default function TextReader({ text, reader }) {
     const handleClick = () => {
         if (!synth.speaking) {
             speechSynthesis.speak(utterance);
+            console.log("uterance:", utterance)
         }
     }
 
@@ -49,23 +59,48 @@ export default function TextReader({ text, reader }) {
     )
 }
 
-function setUtteranceByReader(reader, utterance, synth, lang) {
+function setUtteranceByReader(reader, synth, lang,text) {
     const voices = synth.getVoices();
-    switch (reader) {
-        case "restaurant":
-            utterance.voice = voices[lang == "en" ? 33 : 29]
-            utterance.pitch = lang == "en" ? 2 : 2
-            utterance.rate = lang == "en" ? .8 : .7
-            break;
-        case "aunt_house":
-            utterance.voice = voices[lang == "en" ? 33 : 29]
-            utterance.pitch = lang == "en" ? 1.4 : 1.5
-            utterance.rate = lang == "en" ? .8 : .7
-            break;
-        default:
-            utterance.voice = voices[lang == "en" ? 33 : 52]
-            utterance.pitch = lang == "en" ? 2 : 2
-            utterance.rate = lang == "en" ? .8 : .8
-            break;
+    const acceptedLangs = ["es-ES","es-US","en-US"]
+    let dictionaryOfVoices = {
+        "es":[],
+        "en":[],
     }
+
+    voices.forEach((voice) => {
+        if(acceptedLangs.includes(voice.lang)) {
+            dictionaryOfVoices[voice.lang.slice(0,2)].push(voice)
+        }
+    })
+
+    let voice = dictionaryOfVoices[lang][0]
+
+    if(!voice) {
+        voice = voices[0]
+    }
+
+    let utterance = new SpeechSynthesisUtterance();
+    utterance.text = text
+    utterance.voice = voice
+    utterance.pitch = 1.5
+    utterance.rate = .8
+    
+    // switch (reader) {
+    //     case "restaurant":
+    //         utterance.pitch = lang == "en" ? 2 : 2
+    //         utterance.rate = lang == "en" ? .8 : .7
+    //         break;
+    //     case "aunt_house":
+    //         console.log("aunt house")
+    //         utterance.pitch = lang == "en" ? 1.4 : 1.5
+    //         utterance.rate = lang == "en" ? .8 : .7
+    //         break;
+    //     default:
+    //         utterance.pitch = lang == "en" ? 1.5 : 2.5
+    //         utterance.rate = .8
+    //         break;
+    // }
+
+    return utterance
+
 }
