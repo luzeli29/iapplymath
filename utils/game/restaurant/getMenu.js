065890ddbc8aa0ab1c2af899a2366aad8f04031b
Angel Higueros <45275116+angelhigueros@users.noreply.github.com>
2023-05-05 19:40:25 -0600

    fix(): make water a default choice for drinks

--- utils/game/restaurant/getMenu.js
+++ utils/game/restaurant/getMenu.js
@@ -1,17 +1,28 @@
 import menuOptions from "@public/text/menuOptions"
 
 function getMenu () {
+    const { mainDish, drink, dessert } = menuOptions
+    const drinks = shuffleMenuArray(drink.length, 0)
+    drinks.splice(drinks.indexOf(0), 1)
 
     const menu = {
-        mainDish: shuffleMenuArray(menuOptions.mainDish.length, 0).slice(0,5) || [0,1,2,3,4],
-        drink: shuffleMenuArray(menuOptions.drink.length, 0).slice(0,3) || [0,1,2],
-        dessert: shuffleMenuArray(menuOptions.dessert.length, 0).slice(0,3) || [0,1,2],
+        mainDish: shuffleMenuArray(mainDish.length, 0).slice(0,5) || [0,1,2,3,4],
+        drink: shuffleArray([0, ...drinks.slice(0,2)]) || [0,1,2],
+        dessert: shuffleMenuArray(dessert.length, 0).slice(0,3) || [0,1,2],
     }
 
     return menu
 }
 
 export default getMenu 
+
+const shuffleArray = (arr) => {
+for (let i = arr.length - 1; i > 0; i--) {
+    const randomIndex = Math.floor(Math.random() * (i + 1));
+    [arr[i], arr[randomIndex]] = [arr[randomIndex], arr[i]];
+}
+return arr;
+}
 
 const shuffleMenuArray = (size, offset) => {
     var arr = Array.from(Array(size).keys())
