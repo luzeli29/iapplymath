d12c2b2fbae4a5e699b9c7290e1d106fdfa94806
johnleh <johnleh@live.unc.edu>
2023-07-20 14:43:58 -0400

    fixed issue

--- utils/game/auntHouse/loadAuntHouseRecipes.js
+++ utils/game/auntHouse/loadAuntHouseRecipes.js
@@ -6,6 +6,12 @@ const loadAuntHouseRecipes = async () => {
 
     let recipes = await loadJSONFromUrl('utils/game/recipes/recipeData/recipes.json')
     recipes = recipes.recipe
+    
+    //Filter to only AH Recipes
+    Object.keys(recipes).forEach(key => {
+        if (!recipes[key].showInAuntHouse) delete recipes[key];
+    });
+
     console.log(Object.entries(recipes))
     const ingredients = await loadJSONFromUrl('utils/game/recipes/recipeData/ingredients.json')
     const servingTypes = await loadJSONFromUrl('utils/game/recipes/recipeData/servingTypes.json')
