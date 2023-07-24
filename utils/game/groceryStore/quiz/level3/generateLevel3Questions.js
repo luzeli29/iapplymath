caffaad749d3f365e4915f000a0ec72c3027c02a
johnleh <johnleh@live.unc.edu>
2023-07-23 20:52:35 -0400

    level 3 and images

--- utils/game/groceryStore/quiz/level3/generateLevel3Questions.js
+++ utils/game/groceryStore/quiz/level3/generateLevel3Questions.js
@@ -38,7 +38,7 @@ const generateLevel3Questions = (recipes,questionType,randomGenerator) => {
 const strawberryLimeadeQuestions = (questions,recipe,randomGenerator) => {
     const q1num1 = randomGenerator.randomInt(10,31)
     const q1num2 = randomGenerator.randomInt(5,9)
-    const q1Answer = 
+    const q1Answer = Math.round((((q1num1*1.5)/q1num2)*5.49+ Number.EPSILON) * 100) / 100
     //Unit price question
     questions.push(createGameQuestion(
         {
@@ -283,12 +283,173 @@ const tomatilloSauceAndTortillaChipsQuestions = (questions,recipe,randomGenerato
 }
 
 const mangoJuiceQuestions = (questions,recipe,randomGenerator) => {
+    const q1num1 = 6*randomGenerator.randomInt(1,6)
+    const q1Answer = Math.round((((8*q1num1)/16)*3.79 + Number.EPSILON) * 100) / 100
+    questions.push(createGameQuestion(
+        {
+            en: 'A 16 ounce bag of mango chunks costs $3.79. If you want to make '+q1num1+' servings of mango juice, how much money do you need to spend on mango chunks? Round up to the nearest hundredth. Hint: 1 cup = 8 ounces.',
+            es: 'Una bolsa de 16 onzas de trozos de mango cuesta $3.79. Si quieres hacer '+q1num1+' porciones de jugo de mango, ¿cuánto dinero necesitas gastar en trozos de mango? Redondea al centésimo más cercano. Sugerencia: 1 taza = 8 onzas.'
+        },
+        q1Answer,
+        [
+            {
+                en: 'Total ounces = cups per 1 serving * ounces per 1 cup * servings needed\nTotal bags = total ounces / ounces per 1 bag\nTotal price = total bags * price per 1 bag',                
+                es: 'Onzas totales = tazas por 1 porción * onzas por 1 taza * porciones necesarias\nBolsas totales = onzas totales / onzas por 1 bolsa\nPrecio total = bolsas totales * precio por 1 bolsa'
+            },
+            {
+                en: 'Total ounces = 1 * 8 * '+q1num1+' = “x”\nTotal bags = “x” / 16 = “y”\nTotal price = “y” * 3.79 = ' + q1Answer,                
+                es: 'Onzas totales = 1 * 8 * '+q1num1+' = “x”\nBolsas totales = “x” / 16 = “y”\nPrecio total = “y” * 3.79 = ' + q1Answer
+            }
+        ],
+        'money',
+        null,
+        null
+    ))
+
+    const q2num1 = 9*randomGenerator.randomInt(2,8)
+    const q2Answer = Math.round((((4*q1num1)/36)*2.45 + Number.EPSILON) * 100) / 100
+    questions.push(createGameQuestion(
+        {
+            en: 'At the store, a 1 pound (lb) bag of sugar costs $2.45. If you can only buy sugar in these bags, how much money would you need to spend on sugar to make this recipe '+q2num1+' times? Hint: 1 pound (lb) of sugar = 36 tablespoons (tbsp).',
+            es: 'En la tienda, una bolsa de 1 libra (lb) de azúcar cuesta $2.45. Si solo puedes comprar azúcar en estas bolsas, ¿cuánto dinero necesitarías gastar en azúcar para hacer esta receta '+q2num1+' veces? Sugerencia: 1 libra (lb) de azúcar = 36 cucharadas (cda).'
+        },
+        q2Answer,
+        [
+            {
+                en: 'Total tbsp = recipe sugar tbsp * amount of recipes made\nTotal pounds = total tbsp / tbsp per 1 pound\nTotal cost = total pounds * price per 1 pound',
+                es: 'Cucharadas totales = cucharadas de azúcar de receta * cantidad de recetas hechas\nLibras totales = cucharadas totales / cucharadas por 1 libra\nCosto total = libras totales * precio por 1 libra'
+            },
+            {
+                en: 'Total tbsp = 4 * '+q2num1+' = “x”\nTotal pounds = “x” / 36 = “y”\nTotal cost = “y” * 2.45 = ' + q2Answer,                
+                es: 'Cucharadas totales = 4 * '+q2num1+' = “x”\nLibras totales = “x” / 36 = “y”\nCosto total = “y” * 2.45 = ' + q2Answer
+            }
+        ],
+        'money',
+        null,
+        null
+    ))
 }
 
 const OJCitrusCoolerQuestions = (questions,recipe,randomGenerator) => {
+    const q1num1 = randomGenerator.randomInt(8,33)
+    const q1Answer = Math.round((((q1num1*2)/16)*1.29 + Number.EPSILON) * 100) / 100
+    questions.push(createGameQuestion(
+        {
+            en: 'You are following this recipe and want to make '+q1num1+' servings of OJ Citrus Cooler. If sugar costs $1.29 for 1 cup, how much money do you need to spend on sugar? Hint: 1 cup = 16 tablespoons (tbsp).',
+            es: 'Estás siguiendo esta receta y quieres hacer '+q1num1+' porciones de OJ Citrus Cooler. Si el azúcar cuesta $1.29 por 1 taza, ¿cuánto dinero necesitas gastar en azúcar? Sugerencia: 1 taza = 16 cucharadas (cda).'
+        },
+        q1Answer,
+        [
+            {
+                en: 'Total sugar tbsp = servings needed * sugar tbsp per 1 serving\nTotal cups = Total sugar tbsp / tbsp per 1 cup  (round up)\nTotal cost = total cups * price per 1 cup',
+                es: 'Cucharadas totales de azúcar = porciones necesitadas * cucharadas de azúcar por 1 porción\nTazas totales = Cucharadas totales de azúcar / cucharadas por 1 taza (redondear hacia arriba)\nCosto total = tazas totales * precio por 1 taza'
+            },
+            {
+                en: 'Total sugar tbsp = '+q1num1+' * 2 = “x”\nTotal cups = “x” / 16 = “y”  (round up)\nTotal cost = “y” * 1.29 = ' + q1Answer,
+                es: 'Cucharadas totales de azúcar = '+q1num1+' * 2 = “x”\nTazas totales = “x” / 16 = “y”  (redondear hacia arriba)\nCosto total = “y” * 1.29 = ' + q1Answer
+            }
+        ],
+        'money',
+        null,
+        null
+    ))
+
+    const q2num1 = randomGenerator.randomInt(6,29)
+    const q2Answer = Math.ceil(q2num1*1.5*.5)
+    questions.push(createGameQuestion(
+        {
+            en: 'The OJ Citrus Cooler recipe requires 1 ½  cups of pineapple chunks. At the store, pineapple chunks are sold in pounds. If you’re making '+q2num1+', how many pounds of pineapple chunks do you need to buy for the recipe? Hint: 1 pound of pineapple chunks = 2 cups.',
+            es: 'La receta de OJ Citrus Cooler requiere 1 ½ tazas de trozos de piña. En la tienda, los trozos de piña se venden en libras. Si estás haciendo '+q2num1+', ¿cuántas libras de trozos de piña necesitas comprar para la receta? Sugerencia: 1 libra de trozos de piña = 2 tazas.'
+        },
+        q2Answer,
+        [
+            {
+                en: 'Total cups = servings needed * pineapple cups per 1 serving\nTotal pounds = total cups / cups per 1 pound',                
+                es: 'Tazas totales = porciones necesitadas * tazas de piña por 1 porción\nLibras totales = tazas totales / tazas por 1 libra'
+            },
+            {
+                en: 'Total cups = '+q2num1+' * 1 ½ = “x”\nTotal pounds = “x” / 2 = ' + q2Answer,
+                es: 'Tazas totales = '+q2num1+' * 1 ½ = “x”\nLibras totales = “x” / 2 = ' + q2Answer
+            }
+        ],
+        'wholeNumber',
+        null,
+        null
+    ))
 }
 
 const lentilSoupQuestions = (questions,recipe,randomGenerator) => {
+    const q1num1 = randomGenerator.randomInt(3,9)
+    const q1Answer = Math.round(((q1num1/.75)+ Number.EPSILON) * 100) / 100
+    questions.push(createGameQuestion(
+        {
+            en: 'You have $'+q1num1+' to spend on onions. If one cup of onions costs $0.75, How many cups of onions can you buy with your budget? Round down to the nearest whole number.',
+            es: 'Tienes $'+q1num1+' para gastar en cebollas. Si una taza de cebolla cuesta $0.75, ¿cuántas tazas de cebolla puedes comprar con tu presupuesto? Redondea al número entero más cercano.'
+        },
+        q1Answer,
+        [
+            {
+                en: 'Total cups = budget / price per 1 cup',
+                es: 'Tazas totales = presupuesto / precio por 1 taza'
+            },
+            {
+                en: 'Total cups = '+q1num1+' / 0.75 = ' + q1Answer,
+                es: 'Tazas totales = '+q1num1+' / 0.75 = ' + q1Answer
+            }
+        ],
+        'money',
+        null,
+        null
+    ))
+
+    const q2num1 = randomGenerator.randomInt(2,7)
+    const q2Answer = Math.round(((q2num1*6*.5)*2.69+ Number.EPSILON) * 100) / 100
+    questions.push(createGameQuestion(
+        {
+            en: 'The lentil soup recipe requires 6 cups of lentils. At the store, you can buy a bag of lentils that contains 2 cups and costs $2.69. How much money do you need to buy enough bags of lentils to make the recipe '+q2num1+' times?',
+            es: 'La receta de sopa de lentejas requiere 6 tazas de lentejas. En la tienda, puedes comprar una bolsa de lentejas que contiene 2 tazas y cuesta $2.69. ¿Cuánto dinero necesitas gastar para comprar suficientes bolsas de lentejas para hacer la receta '+q2num1+' veces?'
+        },
+        q2Answer,
+        [
+            {
+                en: 'Total cups = number of recipes * cups per 1 recipe\nTotal bags = total cups / cups per 1 bag\nTotal cost = total bags * price per 1 bag',
+                es: 'Tazas totales = número de recetas * tazas por 1 receta\nBolsas totales = tazas totales / tazas por 1 bolsa\nCosto total = bolsas totales * precio por 1 bolsa'
+            },
+            {
+                en: 'Total cups = '+q2num1+' * 6 = “x”\nTotal bags = “x” / 2 = “y”\nTotal cost = “y” * 2.69 = ' + q2Answer,                
+                es: 'Tazas totales = '+q2num1+' * 6 = “x”\nBolsas totales = “x” / 2 = “y”\nCosto total = “y” * 2.69 = ' + q2Answer
+            }
+        ],
+        'money',
+        null,
+        null
+    ))
+
+    const q3arr1 = [6.99, 8.99]
+    const q3arr2 = [10.99, 11.99, 12.99]
+    const q3num1 = q3arr1[randomGenerator.randomInt(0,q3arr1.length)]
+    const q3num2 = q3arr2[randomGenerator.randomInt(0,q3arr2.length)]
+    const q3Answer = Math.round(((q2num1*6*.5)*2.69+ Number.EPSILON) * 100) / 100
+    questions.push(createGameQuestion(
+        {
+            en: 'At the grocery store, you find two brands of extra virgin olive oil. The Mediterranean brand  offers a 16 fl. oz. bottle for $'+q3num1+' and the Italian Selection brand offers a 24 fl. oz. bottle for $'+q3num2+'. What is the price per unit of the cheaper brand? Round to the nearest hundredth. Hint: The unit here is 1 fluid ounce (fl oz).',
+            es: 'La receta de sopa de lentejas requiere 6 tazas de lentejas. En la tienda, puedes comprar una bolsa de lentejas que contiene 2 tazas y cuesta $2.69. ¿Cuánto dinero necesitas gastar para comprar suficientes bolsas de lentejas para hacer la receta '+q2num1+' veces?'
+        },
+        q2Answer,
+        [
+            {
+                en: 'Total cups = number of recipes * cups per 1 recipe\nTotal bags = total cups / cups per 1 bag\nTotal cost = total bags * price per 1 bag',
+                es: 'Tazas totales = número de recetas * tazas por 1 receta\nBolsas totales = tazas totales / tazas por 1 bolsa\nCosto total = bolsas totales * precio por 1 bolsa'
+            },
+            {
+                en: 'Total cups = '+q2num1+' * 6 = “x”\nTotal bags = “x” / 2 = “y”\nTotal cost = “y” * 2.69 = ' + q2Answer,                
+                es: 'Tazas totales = '+q2num1+' * 6 = “x”\nBolsas totales = “x” / 2 = “y”\nCosto total = “y” * 2.69 = ' + q2Answer
+            }
+        ],
+        'money',
+        null,
+        null
+    ))
 }
 
 export default generateLevel3Questions
